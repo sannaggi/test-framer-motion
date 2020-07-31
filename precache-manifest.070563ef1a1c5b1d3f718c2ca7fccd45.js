71d1631b3c62c09772213e12ea39f053bfb1251f
Xyon <albion0211@gmail.com>
2020-07-31 20:00:18 +0700

    Updates

---
+++ precache-manifest.070563ef1a1c5b1d3f718c2ca7fccd45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c906de39e5ce4870403ea40046f108f",
+    "url": "/test-framer-motion/index.html"
+  },
+  {
+    "revision": "9516277942e15b0d272b",
+    "url": "/test-framer-motion/static/css/main.f6d0fc12.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/test-framer-motion/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/test-framer-motion/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9516277942e15b0d272b",
+    "url": "/test-framer-motion/static/js/main.627c0b7a.chunk.js"
+  },
+  {
+    "revision": "64c5a20e56bd206b1001",
+    "url": "/test-framer-motion/static/js/runtime-main.c588ad0d.js"
+  }
+]);
